e24ec202598ba6f0d7e3c41f1ba407604e6f5ef2
Harun MISTIK <56248277+4nat@users.noreply.github.com>
2020-01-30 11:01:20 +0300

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+cd
+clear
+rm -rf ~/Reborn/sms.py /data/data/com.termux/files/usr/bin/Reborn
+rm -rf /data/data/com.termux/files/usr/bin/Reborn
+rm -rf /data/data/com.termux/files/usr/bin/Reborn
+clear
+cd
+rm -rf Reborn
+clear
+cd
+echo "Uninstalled!!"
