f03acfb5d706c20cad1981b5e9cde3e2ed3485f8
Harun MISTIK <56248277+4nat@users.noreply.github.com>
2020-01-30 10:30:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,54 +1,33 @@
 #!/bin/bash
 clear
-
+if [[ -s update.4nat ]];then
+echo "All Requirements Found...."
+echo -e "\e[1;32m Reborn \e[0m"
+echo "Press Enter To Continue"
+read a1
+clear
+else
+echo 'Installing Requirements....'
+echo .
+toilet -f mono12 -F border 4NAT
 echo -e "\e[4;34m 1- Termux \e[0m"
 echo -e "\e[4;34m 2- Other Linux OS. \e[0m"
 read numb
 if [ $numb = "1" ]
 then
+
 	pkg install python
 	pkg install python3
 	pkg install python3-pip
 	pkg install dos2unix
 	pip3 install requests
 	pip3 install colorama
+        pip3 install -r requirements.txt
 	cp ~/Reborn/sms.py /data/data/com.termux/files/usr/bin/Reborn
 	dos2unix /data/data/com.termux/files/usr/bin/Reborn
 	chmod 777 /data/data/com.termux/files/usr/bin/Reborn
-	Reborn
-else
-	if [ $numb = "2" ]
-	then
 
-		if [ "$(whoami)" != 'root' ]; then
-			echo "Non-root!! (sudo sh ~/Reborn/start.sh)"
-			exit
-		else
-			apt install python3 python3-pip dos2unix
-			pip3 install requests
-			pip3 install colorama
-			cp ~/Reborn/sms.py /usr/bin/Reborn
-			dos2unix /usr/bin/Reborn
-			chmod 777 /usr/bin/
-			Reborn
-		fi
-	else
-		echo "Inavlid İnput!!!"
-	fi
-fi
-clear
-echo -e "\e[1;32m Reborn \e[0m"
-echo "Press Enter To Continue"
-read a1
-if [[ -s update.4nat ]];then
-echo "All Requirements Found...."
-else
-echo 'Installing Requirements....'
-echo .
-pkg install pip2
-pkg install pip3
-apt install python3-pip
-pip3 install -r requirements.txt
+
 echo Made By 4NAT >update.4nat
 echo Requirements Installed....
 echo Press Enter To Continue...
@@ -59,6 +38,7 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
+else
 figlet Reborn
 toilet -f mono12 -F border 4NAT
 echo -e "\e[4;34mCreated By 4NAT \e[0m"
@@ -116,4 +96,23 @@ echo "Press Enter To Go Home"
 read a3
 clear
 fi
-done
+	if [ $numb = "2" ]
+	then
+
+		if [ "$(whoami)" != 'root' ]; then
+			echo "Non-root!! (sudo sh ~/Reborn/start.sh)"
+			exit
+		else
+			apt install python3 python3-pip dos2unix
+			pip3 install requests
+			pip3 install colorama
+			cp ~/Reborn/sms.py /usr/bin/Reborn
+			dos2unix /usr/bin/Reborn
+			chmod 777 /usr/bin/
+			Reborn
+		fi
+	else
+		echo "Inavlid İnput!!!"
+	fi
+fi
+
