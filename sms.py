b55ce5fd46f3ca3616ad4e059dd2a9b6b803369f
Harun MISTIK <har.mstk2015@gmail.com>
2020-02-06 12:02:26 +0300

    Add files via upload

--- sms.py
+++ sms.py
@@ -81,11 +81,8 @@ try:
 except Exception:
     pass
 
-
-
-_phone = int(input('Enter Target Number -->> '))
-
 try:
+   _phone = int(input('Enter Target Number -->> '))
    c = urllib.request.urlopen("https://raw.githubusercontent.com/4nat/reader/master/a.txt").read()
    d = urllib.request.urlopen("https://raw.githubusercontent.com/4nat/reader/master/b.txt").read()
    b=int(base64.b64decode(c))
