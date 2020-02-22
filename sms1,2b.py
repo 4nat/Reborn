ced7fa4a861b0f389887ea1ebd1fad812decf94e
Harun MISTIK <har.mstk2015@gmail.com>
2020-02-22 12:09:51 +0300

    Add files via upload

--- sms1,2b.py
+++ sms1,2b.py
@@ -8,7 +8,7 @@ init()
 colors=['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;34m','\033[1;35m','\033[1;36m']
 W='\033[0m'
 def banner():
-(Fore.GREEN + Back.BLACK + Style.BRIGHT + '''
+    (Fore.GREEN + Back.BLACK + Style.BRIGHT + '''
    ___  _   _   ___ _____ 
   /   || \ | | / _ \_   _|
  / /| ||  \| |/ /_\ \| |  
